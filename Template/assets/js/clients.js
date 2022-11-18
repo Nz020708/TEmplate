d2ce32d856840cf87050794a33908f40db29bc51
Naz Mzadeh <100687473+Nz020708@users.noreply.github.com>
2022-11-18 23:47:53 +0400

    fake api fetched

--- Template/assets/js/clients.js
+++ Template/assets/js/clients.js
@@ -1,120 +1,65 @@
-const testimonials=[
+const testimonials = [
     {
-        id:1,
-        imageSrc:'./assets/images/testimonial-user-cover-49.png',
-        comment:'Slate helps you see how many more days you need to work to reach your financial goal for the month and year.',
-        starImgSrc:'./assets/images/Vector.png',
-        starHalfImgSrc:'./assets/images/Vector2.png',
-        name:'Regina Miles',
-        profession:'Designer'
+        id: 1,
+        imageSrc: "./assets/images/testimonial-user-cover-49.png",
+        comment:
+            "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
+        starImgSrc: "./assets/images/Vector.png",
+        starHalfImgSrc: "./assets/images/Vector2.png",
+        name: "Regina Miles",
+        profession: "Designer",
     },
     {
-        id:2,
-        imageSrc:'./assets/images/testimonial-user-cover-50.png',
-        comment:'Slate helps you see how many more days you need to work to reach your financial goal for the month and year.',
-        starImgSrc:'./assets/images/Vector.png',
-        starHalfImgSrc:'./assets/images/Vector2.png',
-        name:'Jane Donald',
-        profession:'Software Developer'
+        id: 2,
+        imageSrc: "./assets/images/testimonial-user-cover-50.png",
+        comment:
+            "Slate helps you see how many more days you need to work to reach your financial goal for the month and year.",
+        starImgSrc: "./assets/images/Vector.png",
+        starHalfImgSrc: "./assets/images/Vector2.png",
+        name: "Jane Donald",
+        profession: "Software Developer",
     },
-    {
-        id:3,
-        imageSrc:'./assets/images/testimonial-user-cover-49.png',
-        comment:'Slate helps you see how many more days you need to work to reach your financial goal for the month and year.',
-        starImgSrc:'./assets/images/Vector.png',
-        starHalfImgSrc:'./assets/images/Vector2.png',
-        name:'Regina Miles',
-        profession:'Designer'
-    },
-    {
-        id:4,
-        imageSrc:'./assets/images/testimonial-user-cover-50.png',
-        comment:'Slate helps you see how many more days you need to work to reach your financial goal for the month and year.',
-        starImgSrc:'./assets/images/Vector.png',
-        starHalfImgSrc:'./assets/images/Vector2.png',
-        name:'Jane Donald',
-        profession:'Software Developer'
-    }
 ];
 
-const cards_container=document.querySelector('.row-card');
-let testimonialContainer = document.querySelector(".test-container");
-
-
-let i = 0;
-let j = testimonials.length;
-let nextBtn = document.getElementById("left-arrow");
-let prevBtn = document.getElementById("right-arrow");
-nextBtn.addEventListener("click", () => {
-  i = (j + i + 1) % j;
-  displayTestimonial();
-});
-prevBtn.addEventListener("click", () => {
-  i = (j + i - 1) % j;
-  displayTestimonial();
-});
-let displayTestimonial = () => {
-  testimonialContainer.innerHTML = `
-  <div class="card">
-  <img src="${testimonials[i].imageSrc}" alt="testimonial">
-  <p class="comment">${testimonials[i].comment}</p>
-  <div class="stars">
-      <img src="${testimonials[i].starImgSrc}" alt="star">
-      <img src="${testimonials[i].starImgSrc}" alt="star">
-      <img src="${testimonials[i].starImgSrc}" alt="star">
-      <img src="${testimonials[i].starImgSrc}" alt="star">
-      <img src="${testimonials[i].starHalfImgSrc}" alt="star">
-  </div>
-  <h5>${testimonials[i].name}</h5>
-  <p>${testimonials[i].profession}</p>
-  </div>
-  `;
-};
-
-window.onload = displayTestimonial;
-
-nextBtn.addEventListener('click',function () {
-    let isOpen=cards_container.classList.contains('slide-in');
-    cards_container.setAttribute('class', isOpen ? 'row-card cont slide-out' : 'row-card cont slide-in');
-})
-prevBtn.addEventListener('click',function () {
-    let isOpen=cards_container.classList.contains('slide-in');
-    cards_container.setAttribute('class', isOpen ? 'row-card cont slide-out' : 'row-card cont slide-in');
-})
+const cards_container = document.querySelector(".row-card");
 
 
 
-async function getApi() { 
-   try{
-    const res=await fetch('https://jsonplaceholder.typicode.com/comments');
-    const data=await res.json();
-    console.log(data);
-    return data.splice(10,15);
-   }
-   catch(error){
-    console.log(error);
-   }
+async function getApi() {
+    try {
+        const res = await fetch("https://testimonialapi.toolcarton.com/api");
+        let data = await res.json();
+        renderComments(data.splice(3, 2));
+    } catch (error) {
+        console.log(error);
+    }
 }
 getApi();
 
-async function renderComments() {
-    let comments= await getApi();
-    comments.forEach(data => {
-        let testm=` <div class="card">
-        <img src="${testimonials[i].imageSrc}" alt="testimonial">
-        <p class="comment">${data.body}</p>
-        <div class="stars">
-            <img src="${testimonials[i].starImgSrc}" alt="star">
-            <img src="${testimonials[i].starImgSrc}" alt="star">
-            <img src="${testimonials[i].starImgSrc}" alt="star">
-            <img src="${testimonials[i].starImgSrc}" alt="star">
-            <img src="${testimonials[i].starHalfImgSrc}" alt="star">
-        </div>
-        <h5>${data.email}</h5>
-        <p>${data.name}</p>
-        </div>
-        `;
-        testimonialContainer.innerHTML=testm;
-    });
+for (let i = 0; i < testimonials.length; i++) {
+    let star = testimonials[i].starImgSrc;
+    let halfStar = testimonials[i].starHalfImgSrc;
+
+    function renderComments(data) {
+        for (let n = 0; n < data.length; n++) {
+            let testm = `<div class="test-container">
+               <div class="card">
+              <img src="${data[n].avatar}" alt="testimonial">
+              <p class="comment">${data[n].message}</p>
+              <div class="stars">
+                  <img src="${star}" alt="star">
+                  <img src="${star}" alt="star">
+                  <img src="${star}" alt="star">
+                  <img src="${star}" alt="star">
+                  <img src="${halfStar}" alt="star">
+              </div>
+              <h5>${data[n].name}</h5>
+               <p>${data[n].designation}</p>
+              </div>
+              </div>
+              `;
+            cards_container.innerHTML += testm;
+        }
+    }
 }
-renderComments();
+
