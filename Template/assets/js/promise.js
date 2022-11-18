acc4dda061c217fb8aababf58d132c186f65ded8
Naz Mzadeh <100687473+Nz020708@users.noreply.github.com>
2022-11-18 23:49:26 +0400

    Delete Template directory

--- Template/assets/js/promise.js
+++
@@ -1,8 +0,0 @@
-let promise = new Promise(function(resolve, reject) {
-    setTimeout(() => resolve("done!"), 4000);
-  });
-  
-  // resolve runs the first function in .then
-  promise.then(
-   result=> console.log(`Success: ${result}`)) // shows "done!" after 1 second
-// error=> console.log(`Error: ${error}`));
